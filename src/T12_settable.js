84cdc1fdea41c5de148c0e5a3789a8432dff2559
k23096 <k23096kk@aitech.ac.jp>
2024-12-25 13:21:26 +0900

    Update T12_settable.js

--- src/T12_settable.js
+++ src/T12_settable.js
@@ -2,34 +2,34 @@ const gyou=9
 const retu=3
 const tbl = document.createElement("table");
 const tblBody = document.createElement("tbody");
-const row = document.createElement("tr");
+let row = document.createElement("tr");
 const gengo=["増加過程","減少過程","平均値"]
 //最初の宣言
-const cell = document.createElement("td");
-const cellText = document.createTextNode(`\sVp\nVg\s`);
-cell.appendChild(cellText);
-row.appendChild(cell);
 
 for (let i = -1; i < retu; i++) {
 
-  const cell = document.createElement("td");
+  let cell = document.createElement("td");
   if(i<0){
-  const cellText = document.createTextNode(`__Vp`);
-  }else{
-  const cellText = document.createTextNode(`${i*20+230}.0V`);
-  }
+  let cellText = document.createTextNode(`__Vp`);
   cell.appendChild(cellText);
+  }else{
+  let cellText = document.createTextNode(`${i*20+230}.0V`);
+  cell.colSpan="3"
+  cell.appendChild(cellText);
+  }
   row.appendChild(cell);
 }
 tblBody.appendChild(row);
-for (let i = -1; i < retu; i++) {
+row = document.createElement("tr");
+for (let i = -1; i < retu*3; i++) {
   const cell = document.createElement("td");
   if(i<0){
-  const cellText = document.createTextNode(`__Vp`);
+  const cellText = document.createTextNode(`Vg__`);
+  cell.appendChild(cellText);
   }else {
   const cellText = document.createTextNode(gengo[i%3]);
-  }
   cell.appendChild(cellText);
+  }
   row.appendChild(cell);
 }
 tblBody.appendChild(row);
@@ -37,12 +37,17 @@ for (let i = 0; i < gyou; i++) {
     // 表の行を作成
     const row = document.createElement("tr");
 
-    for (let j = 0; j < retu; j++) {
+    for (let j = -1; j < retu*3; j++) {
       // <td> 要素とテキストノードを作成し、テキストノードを
       // <td> の内容として、その <td> を表の行の末尾に追加
       const cell = document.createElement("td");
-      const cellText = document.createTextNode(`${i}、${j}`);
+      if(j<0){
+      const cellText = document.createTextNode(`${-20+i*2}.0V`);
       cell.appendChild(cellText);
+      }else {
+      const cellText = document.createTextNode(`${i}、${j}`);//返り値　反映
+      cell.appendChild(cellText);
+      }
       row.appendChild(cell);
     }
 
