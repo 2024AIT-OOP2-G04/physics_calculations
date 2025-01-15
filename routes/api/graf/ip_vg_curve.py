c482064873bd7020351d1e66880462eb1fdf5e8c
k23096 <k23096kk@aitech.ac.jp>
2025-01-15 23:33:03 +0900

    add ip_vp hyouji

--- routes/api/graf/ip_vg_curve.py
+++ routes/api/graf/ip_vg_curve.py
@@ -1,8 +1,9 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import json
-import vp_vg_curve
-from flask import Flask, render_template, jsonify, request
+#from flask import Flask
+
+#app = Flask(__name__)
 def bibun(fx):#多項式を微分する(多項式はx0*x^3+x1*x^2+..+x3の形とし、fx[0]=x0,fx[1]=x1... と関連付ける
     dfx=[0]*(len(fx)-1)
     for i in range(len(fx)-1):
@@ -34,7 +35,7 @@ def jissuukai(fx,c):#実数解を求める
                 break
     return kai
 
-#@app.route("/ip_vg", methods=["GET"])
+#@app.route("/ip_vg")
 def ip_vg():
     s='{"input_ip_vg":{"vp_230":[0,0.10,0.5,1.48,3.30,6.23,10.05,15.30,21.75],"vp_250":[0.05,0.35,1.10,2.63,4.98,8.38,12.83,18.45,25.55],"vp_270":[0.25,0.83,2.03,4.08,6.95,10.88,15.83,21.98,29.53]}}'
     data = json.loads(s)
@@ -64,21 +65,40 @@ def ip_vg():
         ips[1][j]=jissuukai(fx2,dousaY-5+j*5)
         ips[2][j]=jissuukai(fx3,dousaY-5+j*5)
     t=[]
+    t2=[]
+    dousaf=[]
     for num in [round(num * 0.1, 1) for num in range(-200,-10)]:
         t.append(num)
-     
+        if(num<-3.5):
+            t2.append(num)
+        if((num<-8+4.2) and (num>-8-4.2)):
+            dousaf.append(dousaY)
+        else:
+            dousaf.append(-1) 
+
+
     y1 = np.poly1d(fx1)(t)
     y2 = np.poly1d(fx2)(t)
     y3 = np.poly1d(fx3)(t)
-    ya=np.poly1d(y)(t)
-    plt.plot(t,y3, label='3')
-    plt.plot(t,y2, label='2')
-    plt.plot(t,y1, label='1')
-    plt.plot(t,ya, label='a')
-    plt.legend()
-    plt.show()
-    thisjsdata={"x":t,"vp230":y1,"vp250":y2,"vp270":y3,"sessen":ya,"dousaten":dousaY,"consen":consen}
-    return json.dumps(thisjsdata)
+    ya=np.poly1d(y)(t2)
+    
+    #plt.plot(t,y3, label='3')
+    #plt.plot(t,y2, label='2')
+    #plt.plot(t,y1, label='1')
+    #plt.plot(t,ya, label='a')
+    #plt.legend()
+    #plt.show()
+    print(dousaf)
+    thisjsdata={"x":t,"vp230":y1,"vp250":y2,"vp270":y3,"sessen":ya,"dousaten":dousaY,"consen":consen,"dousaf":dousaf}
 
+    return json.dumps(thisjsdata, cls=NumpyArrayEncoder)
+
+
+class NumpyArrayEncoder(json.JSONEncoder):
+    def default(self,obj):
+        if isinstance(obj,np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self,obj)
+    
 if __name__ == "__main__":
     ip_vg()
